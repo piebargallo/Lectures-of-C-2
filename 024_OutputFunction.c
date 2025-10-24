ab056070226d6d9786510309a98451f09cf242ab
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-10-24 14:47:08 +0200

    Add files via upload

---
+++ 024_OutputFunction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 2;
+	int b = 2;
+	int sum = 0;
+	
+	printf("My message!\n");
+	
+	sum = a + b;
+	printf("a = %d, b = %d, sum = %d", a, b, sum);
+	return 0;
+}
