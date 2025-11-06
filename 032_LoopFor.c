6082b9a9eebbad1545b4666560cf98b759a9423f
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-11-06 18:31:38 +0100

    Add files via upload

---
+++ 032_LoopFor.c
@@ -0,0 +1,16 @@
+/*
+for (inicialización; condición; actualización) {
+  // cuerpo del bucle
+}
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    int i = 1;
+    for (i; i <= 4; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
